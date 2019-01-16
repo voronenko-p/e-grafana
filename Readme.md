99cbd8e80930a5b028ee78b6ce8d994e4d8da44a
Vyacheslav Voronenko <git@voronenko.info>
2019-01-16 23:02:06 +0100

    Adjusted readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,1 @@
-curl -H "Authorization: Bearer eyJrIjoiQURsQ2VRUDBOWWRlNVMzbElkajg2Z0ZVd3oxSlZhTkEiLCJuIjoiZS1ncmFmYW5hIiwiaWQiOjF9" http://10.9.0.138/grafana/api/dashboards/home
+!plugin config SaGrafana {'server_address': 'http://10.9.0.138/grafana', 'token': 'eyJrIjoiQURsQ2VRUDBOWWRlNVMzbElkajg2Z0ZVd3oxSlZhTkEiLCJuIjoiZS1ncmFmYW5hIiwiaWQiOjF9'}
--- sagrafana.py
+++ sagrafana.py
@@ -2,21 +2,27 @@ import os
 import pprint
 from datetime import datetime, timedelta
 
-from errbot import Message, webhook
+from errbot import Message, webhook, arg_botcmd
 from errbot import botcmd, BotPlugin
 from grafanahelper import GrafanaHelper
+
 
 def get_ts():
   now = datetime.now()
   return '%s.%d' % (now.strftime('%Y%m%d-%H%M%S'), now.microsecond)
 
+
 class SaGrafana(BotPlugin):
 
   def get_configuration_template(self):
     return {'server_address': 'https://host/grafana',
-            'token':'eyJrIjoicmNveFpac0tBZm81YzFrMDRNdWVQelRaN3VEOG5tblMiLCJuIjoiZS1ncmFmYW5hIiwiaWQiOjF9'}
+            'token': 'eyJrIjoicmNveFpac0tBZm81YzFrMDRNdWVQelRaN3VEOG5tblMiLCJuIjoiZS1ncmFmYW5hIiwiaWQiOjF9'}
 
   @botcmd(template="grafana_dashboards_list")
-  def grafana_dashboards(self, msg, args):
-    helper= GrafanaHelper(grafana_server_address=self.config['server_address'], grafana_token=self.config['token'])
-
+  @arg_botcmd('filter_tag', type=str)
+  def grafana_dashboards(self, filter_tag=None):
+    # if you send it !grafana dashboards --tag Bot
+    helper = GrafanaHelper(grafana_server_address=self.config['server_address'],
+                           grafana_token=self.config['token'])
+    result = helper.get_dashboards(tag=filter_tag)
+    return {'dashboards': result}
